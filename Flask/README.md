8562938406c51c19268c903fea3cbca93403371a
Mohamed refaat tolba tohamy ismail Elsherbini <ismail.mohamed2065@gmail.com>
2022-10-10 21:40:10 +0000

    sito copiato

---
+++ Flask/README.md
@@ -0,0 +1,13 @@
+## istruzioni per l'utilizzo
+
+```bash 
+cd Flask && pip install -r requirements.txt
+```
+
+## Eseguire l'APP
+
+```bash
+python main.py
+```
+
+apri il webserver su questo link `http://127.0.0.1:5000`
---
+++ Flask/website/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
+
+def create_app():
+    app = Flask(__name__, static_url_path='/assets')
+    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
+
+    from .views import views
+
+    app.register_blueprint(views, url_prefix='/')
+
+    return app
+
